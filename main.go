4b14eaa950db0bdef93bdc15b42b7bd5e77c5abb
Jian Han <jianhan@mail.com>
2018-05-30 20:12:49 +1000

    [DEV] Finished scrape categories

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"github.com/jianhan/goscraper/scraper"
-	"github.com/sirupsen/logrus"
-)
+import "github.com/jianhan/goscraper/scraper"
 
 func main() {
 	//ncix := scraper.NewNCIXScrapper()
@@ -14,11 +11,17 @@ func main() {
 	//	panic(err)
 	//}
 
-	megabuyAU := scraper.NewMegabuyau()
-	if err := megabuyAU.Scrape(); err != nil {
+	//megabuyAU := scraper.NewMegabuyau()
+	//if err := megabuyAU.Scrape(); err != nil {
+	//	panic(err)
+	//}
+	//if err := scraper.OutputJSONData(megabuyAU); err != nil {
+	//	logrus.Warn(err)
+	//}
+
+	amazon := scraper.NewAmazon()
+	if err := amazon.Scrape(); err != nil {
 		panic(err)
 	}
-	if err := scraper.OutputJSONData(megabuyAU); err != nil {
-		logrus.Warn(err)
-	}
+
 }
